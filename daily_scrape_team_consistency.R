3d4e7bc01e2e633b449e35daacba77eaf7370eea
Bill Petti <billpetti@gmail.com>
2015-05-09 06:08:14 -0400

    basic updates

--- daily_scrape_team_consistency.R
+++ daily_scrape_team_consistency.R
@@ -1,4 +1,5 @@
 #load packages: XML to scrape, dplyr to 
+
 require(XML)
 require(dplyr)
 
@@ -10,6 +11,7 @@ teams<-select(teams, Tm)
 teams<-filter(teams, Tm!="LgAvg")
 
 #create function for scraping all records for all teams in 2015
+
 scrape_results<-function(Tm) {
   url <- paste0("http://www.baseball-reference.com/teams/", Tm, "/2015-schedule-scores.shtml")
   data <- readHTMLTable(url, stringsAsFactors = FALSE)
@@ -18,9 +20,11 @@ scrape_results<-function(Tm) {
 }
 
 #apply the scrape_results function to every team in 2015
+
 results_2015<-teams %>% group_by(Tm) %>% do(scrape_results(.))
 
 #tidy up the data.frame using dplyr
+
 cols<-c(3:5, 9:10)
 results_2015<-results_2015[,cols]
 names(results_2015)<-c("Date", "box", "Team", "R", "RA")
@@ -47,12 +51,15 @@ fg<-readHTMLTable("http://www.fangraphs.com/depthcharts.aspx?position=BaseRuns",
 
 standings<-fg[[15]]
 standings<-standings[-(1:2), c(1:8, 11, 12, 15, 16)]
+standings<-standings %>% arrange(desc(`W%`))
 
 #rename columns
+
 names(standings)<-c("Team", "G", "W", "L","W%", "RDif", "RS/G", "RA/G", "PythagenPat W%", "Wins Above/Below Pythag", "BaseRuns W%", "Wins Above/Below BaseRuns")
 
 #create match table for BRef and FanGraphs team names
-fg_t<-c("Diamondbacks", "Braves", "Orioles", "Red Sox", "Cubs", "White Sox", "Reds", "Indians", "Rockies", "Tigers", "Astros", "Royals", "Angels", "Dodgers", "Marlins", "Brewers", "Twins", "Mets", "Yankees", "Atheltics", "Phillies", "Pirates", "Padres", "Mariners", "Giants", "Cardinals", "Rays", "Rangers", "Blue Jays", "Nationals")
+
+fg_t<-c("Diamondbacks", "Braves", "Orioles", "Red Sox", "Cubs", "White Sox", "Reds", "Indians", "Rockies", "Tigers", "Astros", "Royals", "Angels", "Dodgers", "Marlins", "Brewers", "Twins", "Mets", "Yankees", "Athletics", "Phillies", "Pirates", "Padres", "Mariners", "Giants", "Cardinals", "Rays", "Rangers", "Blue Jays", "Nationals")
 
 teams$FG_teams<-fg_t
 colnames(teams)[1]<-"bref_t"
@@ -64,6 +71,39 @@ VOL$R<-round(VOL$R, 2)
 VOL$RA<-round(VOL$RA, 2)
 colnames(VOL)[1]<-"bref_t"
 
+#calculate percentiles for each team for both R and RA.
+
+VOL<-VOL %>% mutate(percrank = rank(-R)/length(R))
+colnames(VOL)[4]<-"R_Ptile"
+VOL<-VOL %>% mutate(percrank = rank(RA)/length(RA))
+colnames(VOL)[5]<-"RA_Ptile"
+VOL$R_Ptile<-round(VOL$R_Ptile, 2)
+VOL$RA_Ptile<-round(VOL$RA_Ptile, 2)
+
 #join VOL scores to standings, via the teams table
+
 standings<-left_join(standings, teams, by = c("Team" = "FG_teams"))
 standings<-left_join(standings, VOL, by = "bref_t")
+
+#transform columns to numeric
+
+attach(standings)
+standings$L %>% as.numeric()
+standings$`W%` <- standings$`W%` %>% as.numeric()
+standings$RDif <- RDif %>% as.numeric()
+standings$`RS/G` <- `RS/G` %>% as.numeric()
+standings$`RA/G` <- `RA/G` %>% as.numeric()
+standings$`PythagenPat W%`<- `PythagenPat W%` %>% as.numeric()
+standings$`Wins Above/Below Pythag` <- `Wins Above/Below Pythag` %>% as.numeric()
+standings$`BaseRuns W%` <- `BaseRuns W%` %>% as.numeric()
+standings$`Wins Above/Below BaseRuns` <- `Wins Above/Below BaseRuns` %>% as.numeric()
+detach(standings)
+
+#plot R vs. RA volatility (percentiles)
+
+require(ggplot2)
+
+g<-ggplot(standings, aes(x=R_Ptile, y=RA_Ptile, label=bref_t)) 
+g<-g + geom_text(aes(colour=factor(`Wins Above/Below Pythag`)))
+g
+
